c10daee9faf0e9c965336ac720096d3d0dd461ab
amanmi625 <aman9063mishra@gmail.com>
2025-12-17 10:57:32 +0530

    Deployment preparation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import notesRoutes from "./routes/notes.routes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.middlewares.js";
@@ -9,23 +10,32 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 connectDB();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // allow requests from your React dev server
-    methods: ["GET", "POST", "PUT", "DELETE"], // explicitly allow common methods
-    allowedHeaders: ["Content-Type", "Authorization"], // allow headers used by axios
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // allow requests from your React dev server
+      methods: ["GET", "POST", "PUT", "DELETE"], // explicitly allow common methods
+      allowedHeaders: ["Content-Type", "Authorization"], // allow headers used by axios
+      credentials: true,
+    })
+  );
+}
 app.use(express.json());
 
 // Apply rate limiter only to notes route
 app.use("/api/notes", rateLimiter, notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // Custom middleware banane ka tarika
 // app.use((req, res, next) => {
 //   console.log(`Request method is ${req.method} & Request URL is ${req.url}`);
